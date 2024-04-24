67ec30cc5ea2802b01bdd0fb54718e74e45c177a
Sean <ussa.say@gmail.com>
2024-04-23 17:17:54 -0700

    concept design

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,19 @@
 export function App () {
-  return <div>hey</div>
+  return (
+    <div className='editor'>
+      <div className='output'>
+        <div className='menubar'>
+          <strong className='logo'>Lecture Editor</strong>
+          <button className='menubar-btn'>File</button>
+          <button className='menubar-btn'>Edit</button>
+        </div>
+        <div className='preview'></div>
+      </div>
+      <div className='script'>
+        <textarea rows={1}>Test</textarea>
+        <div className='annotation'>Change to View 1</div>
+        <textarea>Test</textarea>
+      </div>
+    </div>
+  )
 }
