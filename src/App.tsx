89bbb4b409de413ce978509e9c1f7f436b7c1a0e
Sean <ussa.say@gmail.com>
2024-04-24 19:18:34 -0700

    Make add annotations/text functional

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,44 @@
+import { Fragment, useRef, useState } from 'react'
 import { AddIcon } from './components/AddIcon'
 
+type Annotation =
+  | { type: 'animation'; value: string }
+  | { type: 'set-layout'; value: string }
+  | { type: 'set-background'; value: string }
+type Part = (
+  | { type: 'text'; content: string }
+  | { type: 'annotation'; annotation: Annotation }
+) & { id: number }
+
 export function App () {
+  const nextId = useRef(0)
+  const [parts, setParts] = useState<Part[]>([
+    {
+      id: -1,
+      type: 'text',
+      content: "Hello, I'm Bob. Welcome to CSE 12 lecture recording."
+    },
+    {
+      id: -2,
+      type: 'annotation',
+      annotation: { type: 'set-layout', value: 'Lecturer Only' }
+    },
+    {
+      id: -3,
+      type: 'annotation',
+      annotation: { type: 'animation', value: 'Show Name' }
+    },
+    {
+      id: -4,
+      type: 'annotation',
+      annotation: { type: 'set-background', value: 'Scripps Pier' }
+    },
+    {
+      id: -5,
+      type: 'text',
+      content: 'Today we will talk about things.'
+    }
+  ])
   return (
     <div className='editor'>
       <div className='output'>
@@ -12,45 +50,94 @@ export function App () {
         <div className='preview'></div>
       </div>
       <div className='script'>
-        <textarea rows={1}>Test</textarea>
-        <div className='add-row'>
-          <button className='add-btn'>
-            <AddIcon /> Annotation
-          </button>
-        </div>
-        <div className='annotation first'>
-          <span>
-            Change layout to <strong>Lecturer Only</strong>
-          </span>
-        </div>
-        <div className='add-row'>
-          <button className='add-btn'>
-            <AddIcon /> Annotation
-          </button>
-          <button className='add-btn'>
-            <AddIcon /> Text
-          </button>
-        </div>
-        <div className='annotation'>Show name</div>
-        <div className='add-row'>
-          <button className='add-btn'>
-            <AddIcon /> Annotation
-          </button>
-          <button className='add-btn'>
-            <AddIcon /> Text
-          </button>
-        </div>
-        <div className='annotation last'>
-          <span>
-            Change background to <strong>Scripps Pier</strong>
-          </span>
-        </div>
-        <div className='add-row'>
-          <button className='add-btn'>
-            <AddIcon /> Annotation
-          </button>
-        </div>
-        <textarea>Test</textarea>
+        <h2>Script</h2>
+        {parts.map((part, i) => (
+          <Fragment key={part.id}>
+            {i === 0 && (
+              <div className='add-row'>
+                <button
+                  className='add-btn'
+                  onClick={() =>
+                    setParts(parts =>
+                      parts.toSpliced(
+                        0,
+                        0,
+                        {
+                          id: nextId.current++,
+                          type: 'text',
+                          content: ''
+                        },
+                        {
+                          id: nextId.current++,
+                          type: 'annotation',
+                          annotation: { type: 'animation', value: 'hey' }
+                        }
+                      )
+                    )
+                  }
+                >
+                  <AddIcon /> Annotation
+                </button>
+              </div>
+            )}
+            {part.type === 'text' ? (
+              <textarea placeholder='Start typing...' rows={1}>
+                {part.content}
+              </textarea>
+            ) : (
+              <div
+                className={`annotation ${
+                  parts[i - 1]?.type !== 'annotation' ? 'first' : ''
+                } ${parts[i + 1]?.type !== 'annotation' ? 'last' : ''}`}
+              >
+                <span>
+                  {part.annotation.type === 'animation'
+                    ? 'Play animation'
+                    : part.annotation.type === 'set-background'
+                    ? 'Change background to'
+                    : part.annotation.type === 'set-layout'
+                    ? 'Change layout to'
+                    : ''}{' '}
+                  <strong>{part.annotation.value}</strong>
+                </span>
+              </div>
+            )}
+            <div className='add-row'>
+              <button
+                className='add-btn'
+                onClick={() =>
+                  setParts(parts =>
+                    parts.toSpliced(i + 1, 0, {
+                      id: nextId.current++,
+                      type: 'annotation',
+                      annotation: { type: 'animation', value: 'hey' }
+                    })
+                  )
+                }
+              >
+                <AddIcon /> Annotation
+              </button>
+              {part.type === 'annotation' &&
+                parts[i + 1]?.type === 'annotation' && (
+                  <button
+                    className='add-btn'
+                    onClick={() =>
+                      setParts(parts =>
+                        parts.toSpliced(i + 1, 0, {
+                          id: nextId.current++,
+                          type: 'text',
+                          content: ''
+                        })
+                      )
+                    }
+                  >
+                    <AddIcon /> Text
+                  </button>
+                )}
+            </div>
+          </Fragment>
+        ))}
+        <div className='end'></div>
       </div>
     </div>
   )
