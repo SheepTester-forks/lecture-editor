459fcaf30ab335ab3abdd7190c6e75e0599522a8
Sean <ussa.say@gmail.com>
2024-05-24 23:00:02 -0700

    Animate slide positions

--- src/render/render.ts
+++ src/render/render.ts
@@ -6,6 +6,57 @@ const WIDTH = 1920
 const HEIGHT = 1080
 const PADDING = 20
 
+const TRANSITION_DURATION = 500
+
+function clamp (value: number, min = 0, max = 1): number {
+  if (value > max) {
+    return max
+  }
+  if (value < min) {
+    return min
+  }
+  return value
+}
+
+function easeInOutCubic (t: number): number {
+  t *= 2
+  if (t < 1) {
+    return (t * t * t) / 2
+  }
+  t -= 2
+  return (t * t * t + 2) / 2
+}
+
+function v (
+  t: number,
+  from: number | undefined,
+  to: number | undefined,
+  defaultValue: number
+): number {
+  if (from === undefined) {
+    return to ?? defaultValue
+  } else if (to === undefined) {
+    return from
+  }
+  return from + (to - from) * t
+}
+
+type Rect = { x: number; y: number; width: number; height: number }
+function contain (aspect: number, bounds: Rect): Rect {
+  const width = Math.min(bounds.width, bounds.height * aspect)
+  const height = width / aspect
+  return {
+    x: bounds.x + (bounds.width - width) / 2,
+    y: bounds.y + (bounds.height - height) / 2,
+    width,
+    height
+  }
+}
+
+type Style = Rect & {
+  opacity: number
+}
+
 export function render (
   context: CanvasRenderingContext2D,
   strategy: Strategy,
@@ -25,24 +76,80 @@ export function render (
     HEIGHT - PADDING
   )
 
-  let layout: Layout = 'avatar-only'
+  let previousLayout: Layout | undefined
+  let currentLayout: { layout: Layout; time: number } | undefined
   for (const action of strategy.actions) {
     if (time < action.time) {
       break
     }
     if (action.type === 'set-layout') {
-      layout = action.layout
+      previousLayout = currentLayout?.layout
+      currentLayout = action
     }
   }
 
-  if (layout === 'slide-only') {
+  const layoutStyles: Record<Layout, Partial<Style>> = {
+    'avatar-only': { opacity: 0 },
+    'slide-avatar': {
+      ...contain(1.5 + Math.sin(time / 1000) / 2, {
+        x: PADDING,
+        y: PADDING,
+        width: (WIDTH * 2) / 3 - PADDING * 2,
+        height: HEIGHT - PADDING * 2 - 40
+      }),
+      opacity: 1
+    },
+    'slide-only': {
+      ...contain(1.5 + Math.sin(time / 1000) / 2, {
+        x: PADDING,
+        y: PADDING,
+        width: WIDTH - PADDING * 2,
+        height: HEIGHT - PADDING * 2 - 40
+      }),
+      opacity: 1
+    }
+  }
+  const defaultLayout: Layout = 'avatar-only'
+  const layoutTransition = {
+    from: layoutStyles[previousLayout ?? defaultLayout],
+    to: layoutStyles[currentLayout?.layout ?? defaultLayout],
+    t: easeInOutCubic(
+      clamp((time - (currentLayout?.time ?? -Infinity)) / TRANSITION_DURATION)
+    )
+  }
+  const layoutStyle: Style = {
+    x: v(layoutTransition.t, layoutTransition.from.x, layoutTransition.to.x, 0),
+    y: v(layoutTransition.t, layoutTransition.from.y, layoutTransition.to.y, 0),
+    width: v(
+      layoutTransition.t,
+      layoutTransition.from.width,
+      layoutTransition.to.width,
+      0
+    ),
+    height: v(
+      layoutTransition.t,
+      layoutTransition.from.height,
+      layoutTransition.to.height,
+      0
+    ),
+    opacity: v(
+      layoutTransition.t,
+      layoutTransition.from.opacity,
+      layoutTransition.to.opacity,
+      1
+    )
+  }
+
+  if (layoutStyle.opacity > 0) {
+    context.globalAlpha = layoutStyle.opacity
     context.fillStyle = 'white'
     context.fillRect(
-      PADDING,
-      PADDING,
-      WIDTH - PADDING * 2,
-      HEIGHT - PADDING * 2 - 40
+      layoutStyle.x,
+      layoutStyle.y,
+      layoutStyle.width,
+      layoutStyle.height
     )
+    context.globalAlpha = 1
   }
 
   context.restore()
